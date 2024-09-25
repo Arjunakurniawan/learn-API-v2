b35a25c0b81ee5abc6f2d2c470b1e0e6200527b7
ArjunaKurniawan <arjunakurniawan686@gmail.com>
2024-09-25 16:26:23 +0700

    success make part of the code

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
-const inputTitle = document.getElementById('inputTitle');
-const inputBody = document.getElementById('inputBody');
+const inputTitle = document.getElementById("inputTitle");
+const inputBody = document.getElementById("inputBody");
+const postlist = document.getElementById("postList");
+const button = document.getElementById("buttonSubmit");
 
+function fetchPost() {
+  fetch("https://jsonplaceholder.typicode.com/posts")
+    .then(function (res) {
+      return res.json();
+    })
+    .then(function (post) {
+      let postList = document.getElementById("postList");
+      postList.innerHTML = "";
+
+      for (let i = 0; i < post.length; i++) {
+        let postItem = document.createElement("li");
+        postItem.textContent = `id: ${post[i].id} title: ${post[i].title}`;
+        postList.append(postItem);
+      }
+    })
+    .catch(function (err) {
+      console.error("Error", err);
+    });
+}
+
+button.onclick = function () {};
+
+fetchPost();
